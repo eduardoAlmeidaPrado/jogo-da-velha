c672969131c67bd10b36972def232ade6ccde9db
Eduardo Almeida Prado <eduardoalmeidaprado04@gmail.com>
2025-02-21 16:34:53 -0300

    Configuração de vitória e empate
    Criação das funções que monitoram vitória ou empate no jogo

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,17 @@ const PLAYER_X = "X";
 const PLAYER_O = "O";
 let playerTurn = true; // X = true O = false
 
+const WIN = [
+    [0, 1 ,2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 document.addEventListener("click", (event) => {
     if(event.target.matches(".tile")) {
         start(event.target.id);
@@ -12,6 +23,49 @@ document.addEventListener("click", (event) => {
 function start(id) {
     const tile = document.getElementById(id);
     turn = playerTurn ? PLAYER_X : PLAYER_O;
-    tile.textContent = turn;
-    playerTurn = !playerTurn;
+    tile.classList.add(turn);
+    updateGame(turn);
+}
+
+function updateGame(turn) {
+    const playerWin = WIN.some((comb) => {
+        return comb.every((index) => {
+            return TILES[index].classList.contains(turn);
+        });
+    });
+
+    if(playerWin) {
+        endGame(turn);
+    } else if(isDraw()) {
+        endGame();
+    } else {
+        playerTurn = !playerTurn;
+    }
+}
+
+function isDraw() {
+    let x = 0;
+    let o = 0;
+
+    for(index in TILES) {
+        if(!isNaN(index)) {
+            if(TILES[index].classList.contains(JOGADOR_X)) {
+                x++;
+            }
+    
+            if(TILES[index].classList.contains(JOGADOR_O)) {
+                o++;
+            }
+        }
+    }
+
+    return x + o === 9 ? true : false;
+}
+
+function endGame(playerWin = null) {
+    if(playerWin) {
+        console.log(playerWin + " venceu!");
+    } else {
+        console.log("Empate!");
+    }
 }
